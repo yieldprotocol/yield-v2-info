ee690506c30bc1f4ec4eed82c4e3c6d152e912f0
David Mihal <david@dmihal.com>
2020-11-30 22:39:54 +0000

    Fix some timezone/DST issues

--- lib/format.ts
+++ lib/format.ts
@@ -1,3 +1,8 @@
 import format from 'date-fns/format';
 
-export const formatMaturity = (timestamp: string) => format(new Date(parseInt(timestamp) * 1000), 'MMMM yyyy');
+// Hack to fix some weirdness around timezones & DST.
+// We're only displaying the month so this shouldn't cause problems
+const twentyThreeHrs = 23 * 60 * 60 * 1000;
+
+export const formatMaturity = (timestamp: string) =>
+  format(parseInt(timestamp) * 1000 - twentyThreeHrs, 'MMMM yyyy');
