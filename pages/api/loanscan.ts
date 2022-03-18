4b9790b0d7c82010e1d470f39c935a6b7c3daf17
David Mihal <david@dmihal.com>
2022-03-18 18:49:51 +0200

    Update Loanscan API

--- pages/api/loanscan.ts
+++ pages/api/loanscan.ts
@@ -3,9 +3,17 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { gql } from '@apollo/client';
 
 export const APR_QUERY = gql`
-  query maturities {
-    fydais(orderBy: maturity) {
-      apr
+  query maturities($currentTimestamp: Int!) {
+    seriesEntities(where: { maturity_gt: $currentTimestamp }) {
+      maturity
+      baseAsset {
+        symbol
+      }
+      fyToken {
+        pools {
+          apr
+        }
+      }
     }
   }
 `
@@ -13,7 +21,7 @@ export const APR_QUERY = gql`
 function createApolloClient() {
   return new ApolloClient({
     link: new HttpLink({
-      uri: 'https://api.thegraph.com/subgraphs/name/yieldprotocol/mainnet-staging',
+      uri: 'https://api.thegraph.com/subgraphs/name/yieldprotocol/v2-mainnet',
     }),
     cache: new InMemoryCache(),
   })
@@ -24,32 +32,39 @@ const apr2apy = (apr: number) => Math.pow(Math.E, apr) - 1
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
   try {
     const client = createApolloClient()
-    const result = await client.query({ query: APR_QUERY })
-    const [topAPR, bottomAPR] = result.data.fydais
-      .reduce(([top, bottom]: [number, number], fyDai: any) => {
-        let _top = fyDai.apr > top ? parseFloat(fyDai.apr) : top
-        let _bottom = fyDai.apr > 0 && fyDai.apr < bottom ? parseFloat(fyDai.apr) : bottom
-        return [_top, _bottom]
-      }, [0, 100])
-      .map((apr: number) => apr / 100)
+    const result = await client.query({
+      query: APR_QUERY,
+      variables: { currentTimestamp: Math.floor(Date.now() / 1000) },
+    })
+
+    const pools: { [symbol: string]: { maxAPR: number, minAPR: number } } = {};
+
+    for (const series of result.data.seriesEntities) {
+      if (!pools[series.baseAsset.symbol]) {
+        pools[series.baseAsset.symbol] = { maxAPR: 0, minAPR: 100 };
+      }
+
+      if (parseFloat(series.fyToken.pools[0].apr) < pools[series.baseAsset.symbol].minAPR) {
+        pools[series.baseAsset.symbol].minAPR = parseFloat(series.fyToken.pools[0].apr);
+      }
+      if (parseFloat(series.fyToken.pools[0].apr) > pools[series.baseAsset.symbol].maxAPR) {
+        pools[series.baseAsset.symbol].maxAPR = parseFloat(series.fyToken.pools[0].apr);
+      }
+    }
 
     res.json({
-      lendRates: [
-        {
-          apy: apr2apy(topAPR),    // Annual Percentage Yield, multiplier format
-          apr: topAPR,  // Annual Percentage Rate, multiplier format
-          tokenSymbol: 'DAI', // a symbol of listed currency (3 or 4 capital letters)
-        },
-      ],
-      borrowRates: [
-        {
-          apy: apr2apy(bottomAPR),
-          apr: bottomAPR,
-          tokenSymbol: 'DAI',
-        },
-      ],
+      lendRates: Object.entries(pools).map(([symbol, { maxAPR }]) => ({
+        apy: apr2apy(maxAPR),
+        apr: maxAPR,
+        tokenSymbol: symbol,
+      })),
+      borrowRates: Object.entries(pools).map(([symbol, { minAPR }]) => ({
+        apy: apr2apy(minAPR),
+        apr: minAPR,
+        tokenSymbol: symbol,
+      })),
     })
-  } catch (e) {
+  } catch (e: any) {
     console.error(e)
     res.status(500).json({ error: e.message })
   }
